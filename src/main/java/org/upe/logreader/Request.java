34ad16188acdb7a6b6671792d77c8feceebc28b7
keisylizandra <k22.lizandra@gmail.com>
2024-05-24 12:07:17 -0300

    Update Request class

--- src/main/java/org/upe/logreader/Request.java
+++ src/main/java/org/upe/logreader/Request.java
@@ -1,4 +1,11 @@
 package org.upe.logreader;
 
 public class Request {
+    private static final String recursosGrandesPath = "./Análise/RecursosGrandes.txt";
+    private static final String naoRespondidosNovembroPath = "./Análise/RecursosGrandes.txt";
+    private LogData dataArray;
+    private String status200;
+    private String status400;
+    private String postRequest;
+
 }
