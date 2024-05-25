73be341c1d088c537e5b3f83269ac88c62164eba
devictor8 <joaosalgado605@gmail.com>
2024-05-24 21:25:07 -0300

    varios erros

--- src/main/java/org/upe/logreader/Main.java
+++ src/main/java/org/upe/logreader/Main.java
@@ -8,19 +8,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<LogData> data = LogData.logReader();
+        Request requestData = new Request();
+        requestData.DataAnalysis(LogData.logReader());
         int option;
 
-        for(LogData os : data) {
-            System.out.println(os.getDate());
+        for(String os : requestData.status200) {
+            System.out.println(os);
         }
 
         System.out.println("1 - Recursos grandes respondidos\n2 - Não respondidos\n3 - % de requisições por SO\n4 - Média das requisições POST\n0 - Sair");
         option = sc.nextInt();
 
+        boolean isFileRead = false;
         switch (option) {
             case 0:
                 System.out.println("case 0");
+                break;
+            case 1:
+                if(isFileRead) {
+
+                } 
         }
 
         sc.close();
--- src/main/java/org/upe/logreader/Request.java
+++ src/main/java/org/upe/logreader/Request.java
@@ -1,11 +1,41 @@
 package org.upe.logreader;
 
+import java.util.ArrayList;
+
 public class Request {
     private static final String recursosGrandesPath = "./Análise/RecursosGrandes.txt";
     private static final String naoRespondidosNovembroPath = "./Análise/RecursosGrandes.txt";
-    private LogData dataArray;
-    private String status200;
-    private String status400;
-    private String postRequest;
+    private ArrayList<LogData> dataArray;
+    public ArrayList<String> status200;
+    private ArrayList<String> status400;
+    private ArrayList<String> postRequest;
 
+    public void DataAnalysis(ArrayList<LogData> logData) {
+        this.dataArray = logData;
+
+        for (LogData lineData : this.dataArray) {
+            if (validateDataSuccessfulRequest(lineData)) {
+                status200.add(String.format("%s %s %s", lineData.getStatus(), lineData.getObjectSize(), lineData.getIP()));
+            };
+            if (validateDataUnsuccessfulRequest(lineData)) {
+                status400.add(String.format("%s %s %s", lineData.getStatus(), lineData.getURL(), lineData.getDate()));
+            };
+
+        }
+    }
+    private boolean validateDataSuccessfulRequest(LogData dateLine) {
+        return true;
+    }
+
+    private boolean validateDataUnsuccessfulRequest(LogData dateLine) {
+        return true;
+    }
+
+    public void createStatus200File() {
+
+    }
+
+    public void createStatus400File() {
+
+    }
 }
